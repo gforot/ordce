f89f452c32738b1b31ce5609cf7ec344ea474f21
sca SW <gforot@hotmail.it>
2014-07-15 12:20:18 +0200

    first commit

---
+++ OrdiniCatCe.Gui/ViewModel/MainViewModel.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using GalaSoft.MvvmLight;
+using OrdiniCatCe.Gui.Xxxxxx;
+
+
+namespace OrdiniCatCe.Gui.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        public List<Marche> Marches { get; private set; }
+        public List<RichiesteOrdine> RigheOrdine { get; private set; } 
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
+
+            using (OrdiniEntities db = new OrdiniEntities())
+            {
+                Marches = db.Marche.ToList();
+                RigheOrdine = db.RichiesteOrdine.ToList();
+            }
+        
+        }
+    }
+}
