489b69409dca74414f69048ce748a82bd4e1556b
sca SW <gforot@hotmail.it>
2014-07-22 21:55:13 +0200

    validation

---
+++ OrdiniCatCe.Gui/Validation/EMailValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Controls;
+
+
+namespace OrdiniCatCe.Gui.Validation
+{
+    public class EMailValidator : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            if (string.IsNullOrEmpty((string)value))
+            {
+                return new ValidationResult(false, "Non può essere vuoto");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
