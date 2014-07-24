4aa96b7d9458f7c7acf20039fd7f6d32d57bccab
sca SW <gforot@hotmail.it>
2014-07-24 10:33:19 +0200

    upd

--- OrdiniCatCe.Gui/Constants/AppColors.cs
+++ OrdiniCatCe.Gui/Constants/AppColors.cs
@@ -5,14 +5,37 @@ namespace OrdiniCatCe.Gui.Constants
 {
     public class AppColors
     {
-        public static Color NextActionColor = Colors.MediumSpringGreen;
-        public static Color EnabledButtonColor = Colors.MediumAquamarine;
+        private const byte _alpha = 50;
+
+        public static Color NextActionColor;
+        public static Color EnabledButtonColor;
 
 
         //ROW BACKGROUND
-        public static Color OrdinatoRowBackground = Colors.Orange;
-        public static Color AvvisatoRowBackground = Colors.LightGreen;
-        public static Color ArrivatoRowBackground = Colors.Red;
-        public static Color RitiratoRowBackground = Colors.Turquoise;
+        public static Color OrdinatoRowBackground;
+        public static Color AvvisatoRowBackground;
+        public static Color ArrivatoRowBackground;
+        public static Color RitiratoRowBackground;
+
+        static AppColors()
+        {
+            OrdinatoRowBackground = Colors.Orange;
+            OrdinatoRowBackground.A = _alpha;
+
+            AvvisatoRowBackground = Colors.LightGreen;
+            AvvisatoRowBackground.A = _alpha;
+
+            ArrivatoRowBackground = Colors.Red;
+            ArrivatoRowBackground.A = _alpha;
+
+            RitiratoRowBackground = Colors.Turquoise;
+            RitiratoRowBackground.A = _alpha;
+
+            NextActionColor = Colors.MediumSpringGreen;
+            NextActionColor.A = _alpha;
+
+            EnabledButtonColor = Colors.MediumAquamarine;
+            EnabledButtonColor.A = _alpha;
+        }
     }
 }
