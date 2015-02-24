ce8a23bce574c9cd9ab68915a17c63eb0bbcc361
SCA <gforot@hotmail.it>
2015-02-24 09:50:33 +0100

    migliorato tooltip

---
+++ OrdiniCatCe.Gui/Converters/DatetimeIfExistsConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+
+namespace OrdiniCatCe.Gui.Converters
+{
+    public class DatetimeIfExistsConverter : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if((values == null) || (values.Count()!=2))
+            {
+                return string.Empty;
+            }
+
+            if (!(values[0] is bool))
+            {
+                return string.Empty;
+            }
+
+            if (!(values[1] is DateTime))
+            {
+                return string.Empty;
+            }
+
+            bool exists = (bool)values[0];
+            if (!exists)
+            {
+                return string.Empty;
+            }
+            DateTime dt = (DateTime)values[1];
+            string dtStr = dt.ToShortDateString();
+            return dtStr;
+        }
+
+        public object[] ConvertBack(object value, System.Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ OrdiniCatCe.Gui/Converters/YesNoConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+
+namespace OrdiniCatCe.Gui.Converters
+{
+    public class YesNoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                //se non sto visualizzando lo storico(VisualizzaStorico=false) il testo deve essere "Visualizza Storico".
+                if (!(bool)value)
+                {
+                    return "NO";
+                }
+            }
+
+            return "SI";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
